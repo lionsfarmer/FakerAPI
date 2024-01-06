caf1c4fea020c19ea8bb0645178dc9ba3123aaab
Lions <lionsfarmer@proton.me>
2024-01-06 13:35:59 -0600

    faker function created

---
+++ R/faker.R
@@ -0,0 +1,18 @@
+faker <- function(resource, ..., quantity = 1, locale = "en_US", seed = NULL) {
+  params <- list(
+    ...,
+    quantity = quantity,
+    locale = locale,
+    seed = seed
+  )
+  names(params) <- paste0("_", names(params))
+
+  httr2::request("https://fakerapi.it/api/v1") |>
+    httr2::req_url_path_append(resource) |>
+    httr2::req_url_query(!!!params) |>
+    httr2::req_user_agent("my_package_name (http://my.package.web.site)") |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+}
+
+str(faker("images", width = 300))
